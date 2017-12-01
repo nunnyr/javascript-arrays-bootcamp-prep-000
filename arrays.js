e845def331c835f30d257ef7415e865da801c297
Nunny R <nunnyreyes@gmail.com>
2017-12-01 17:14:53 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`]
+
+function addElementToBeginningOfArray(array, element) {
+  return array.push(element)
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+  return array.shift(element)
+}
